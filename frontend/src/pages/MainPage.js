7b24ec16c2083228b271b73a7a2ff51c4262addd
gochunwoo <youthes2821@naver.com>
2025-06-20 15:50:03 +0900

    프론트 프로젝트 파일구조

---
+++ frontend/src/pages/MainPage.js
@@ -0,0 +1,3 @@
+import React from "react";
+const MainPage = () => <div>메인 페이지</div>;
+export default MainPage;
---
+++ frontend/src/pages/ProductListPage.js
@@ -0,0 +1,3 @@
+import React from "react";
+const ProductListPage = () => <div>상품 목록 페이지</div>;
+export default ProductListPage;
