21e82a7a7a0acf717d6f64989c4ac7b80914f9f2
youngsic901 <youngsic901@gmail.com>
2025-06-25 11:06:52 +0900

    2025.06.25 에러수정

--- backend/src/main/java/com/example/aromadesk/auth/service/MemberLoginService.java
+++ backend/src/main/java/com/example/aromadesk/auth/service/MemberLoginService.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,9 +14,6 @@ public class MemberLoginService implements UserDetailsService{
 
     @Autowired
     private MemberRepository memberRepository;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String memberId) throws UsernameNotFoundException {
